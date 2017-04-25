670992a7a866f716474715caf0003d6732542eaa
Josh White <whitejosh42@gmail.com>
2017-04-25 10:31:11 -0500

    Front page working

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const nunjucks = require('nunjucks');
 const express = require('express');
 const app = express();
+const volleyball = require('volleyball');
 
+app.use(volleyball);
 
 const routes = require('./routes');
+
+app.use(express.static('public'));
+
 app.use('/', routes);
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
-app.use(express.static('public'));
+
 
 
 app.listen(3000, function() {
