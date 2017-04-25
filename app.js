b1a590a7bddb8a19f92ff99b104952f10d6aa495
dbui0051 <dennisd.bui@gmail.com>
2017-04-25 11:19:22 -0400

    twitter update

--- app.js
+++ app.js
@@ -1,66 +1,17 @@
-const express = require('express');
-const chalk = require('chalk');
 const nunjucks = require('nunjucks');
+const express = require('express');
 const app = express();
-const router = express.Router();
+
+
+const routes = require('./routes');
+app.use('/', routes);
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
+app.use(express.static('public'));
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(chalk.blue(output));
-});
-
-// router.use('/', function (request, response) {
-//   console.log('Recieved some request')
-// });
-
-app.use(function (request, response, next) {
-  // console.log(Object.keys(request));
-  console.log(chalk.red(request.method, request.url));
-  next();
-})
-
-app.get('/', function (request, response) {
-  response.render('index', locals);
-});
-
-
-app.use('/news', function (request, response, next) {
-  console.log("Accessing news");
-  next();
-});
-
-app.get('/news', function (request, response, next) {
-  response.send("Heres the news");
-  next();
-});
-
-
-app.use('/news', router);
-
-router.get('/abc', function(request, response, next){
-  response.send("caw, caw, caw")
-});
-
-router.get('/bcd', function(request, response, next){
-  response.send("bawk bawk bawk")
-});
-
-router.get('/cde', function(request, response, next){
-  response.send("gobble gobble gobble")
-});
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...');
--- tweetBank.js
+++ tweetBank.js
@@ -19,31 +19,7 @@ module.exports = { add: add, list: list, find: find };
 
 
 
-// const randArrayEl = function(arr) {
-//   return arr[Math.floor(Math.random() * arr.length)];
-// };
 
-// const getFakeName = function() {
-//   const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
-//   const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
-//   return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
-// };
-
-// const getFakeTweet = function() {
-//   const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
-//   return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
-// };
-
-// for (let i = 0; i < 10; i++) {
-//   module.exports.add( getFakeName(), getFakeTweet() );
-// }
-
-add('Dennis Bui', 'Hello world!');
-add('Josh White', 'Ello mate');
-add('Steve Smith', 'Yarr matey');
-
-console.log(find(["name", 'Steve Smith']));
-console.log(find({"name": 'Dennis Bui'}));
 
 
 
