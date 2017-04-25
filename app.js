b352fab5d6683274a57f37176cf7976bbe09b18e
dbui0051 <dennisd.bui@gmail.com>
2017-04-25 10:40:47 -0400

    update twitter.js

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const chalk = require('chalk');
+const nunjucks = require('nunjucks');
 const app = express();
 const router = express.Router();
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(chalk.blue(output));
+});
 
 // router.use('/', function (request, response) {
 //   console.log('Recieved some request')
@@ -9,12 +28,12 @@ const router = express.Router();
 
 app.use(function (request, response, next) {
   // console.log(Object.keys(request));
-  console.log(request.method, request.url);
+  console.log(chalk.red(request.method, request.url));
   next();
 })
 
 app.get('/', function (request, response) {
-  response.send('Welcome to nothing.');
+  response.render('index', locals);
 });
 
 
@@ -46,3 +65,16 @@ router.get('/cde', function(request, response, next){
 app.listen(3000, function() {
   console.log('Listening on port 3000...');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
